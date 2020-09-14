692ad20702b5e0fe2570cbdaf1cc134682296ed9
markusreynolds1989 <markusreynolds1989@gmail.com>
2020-09-14 13:48:54 -0500

    Game 75 percent, need assets and anim

--- index.js
+++ index.js
@@ -110,208 +110,304 @@ document.addEventListener("mouseup", mouseClickHandler, false);
 
 /*** End Library  ***/
 
-// Start Program
-const pumpkin = new Image(),
-	ghost = new Image(),
-	seedBag = new Image(),
-	dryDirt1 = new Image(),
-	wetDirt1 = new Image(),
-	seed = new Image(),
-	tileState = {
-		empty: "empty",
-		hasSeed: "hasSeed",
-		hasPumpkin: "hasPumpkin",
-	};
+const game = () => {
+	// Start Program
+	const pumpkin = new Image(),
+		ghost = new Image(),
+		seedBag = new Image(),
+		dryDirt1 = new Image(),
+		wetDirt1 = new Image(),
+		seed = new Image(),
+		tileState = {
+			empty: "empty",
+			hasSeed: "hasSeed",
+			hasPumpkin: "hasPumpkin",
+			hasTree: "hasTree",
+			hasStone: "hasStone",
+			isWell: "isWell"
+		};
 
-let tiles = [],
-	ghosts = [],
-	pumpkins = 0,
-	seeds = 1;
+	let tiles = [],
+		ghosts = [],
+		pumpkins = 0,
+		meter,
+		seeds = 1,
+		tick = 0,
+		seconds = 300,
+		score = 0,
+		gameOver;
 
-class Ghost {
-	constructor(x, y, originX, originY) {
-		this.x = x;
-		this.y = y;
-		this.originX = originX;
-		this.originY = originY;
+	class WaterMeter {
+		constructor() {
+			this.fillLevel = -100;
+		}
+
+		draw() {
+			drawRectangeLines(760, 300, 30, 200, "#ffffeb");
+			drawRectangle(761, 500, 28, this.fillLevel, "#4da6ff");
+		}
+
+		drain() {
+			if (this.fillLevel < 0)
+				this.fillLevel += 20;
+		}
+
+		fill() {
+			this.fillLevel = -200;
+		}
 	}
 
-	draw() {
-		drawImage(ghost, this.x, this.y);
-	}
+	class Ghost {
+		constructor(x, y, originX, originY) {
+			this.x = x;
+			this.y = y;
+			this.originX = originX;
+			this.originY = originY;
+		}
 
-	search() {
-		let currentTile = tiles.find((x) => x.state == tileState.hasPumpkin);
-		if (currentTile != undefined) {
-			if (this.x < currentTile.x) {
-				this.x += 0.5;
-			}
-			if (this.y < currentTile.y) {
-				this.y += 0.5;
-			}
-			if (this.x > currentTile.x) {
-				this.x -= 0.5;
-			}
-			if (this.y > currentTile.y) {
-				this.y -= 0.5;
-			}
-			if (this.x == currentTile.x && this.y == currentTile.y) {
-				currentTile.harvest();
-			}
-		} else {
-			if (this.originX > 0 && this.originY > 0) {
-				if (this.x < this.originX) {
-					this.x += 1;
+		draw() {
+			drawImage(ghost, this.x, this.y);
+		}
+
+		search() {
+			let currentTile = tiles.find((x) => x.state == tileState.hasPumpkin);
+			if (currentTile != undefined) {
+				if (this.x < currentTile.x) {
+					this.x += 0.5;
 				}
-				if (this.y < this.originY) {
-					this.y += 1;
+				if (this.y < currentTile.y) {
+					this.y += 0.5;
+				}
+				if (this.x > currentTile.x) {
+					this.x -= 0.5;
+				}
+				if (this.y > currentTile.y) {
+					this.y -= 0.5;
+				}
+				if (this.x == currentTile.x && this.y == currentTile.y) {
+					currentTile.harvest();
 				}
 			} else {
-				if (this.x > this.originX) {
-					this.x -= 1;
-				}
-				if (this.y > this.originY) {
-					this.y -= 1;
+				if (this.originX > 0 && this.originY > 0) {
+					if (this.x < this.originX) {
+						this.x += 1;
+					}
+					if (this.y < this.originY) {
+						this.y += 1;
+					}
+				} else {
+					if (this.x > this.originX) {
+						this.x -= 1;
+					}
+					if (this.y > this.originY) {
+						this.y -= 1;
+					}
 				}
 			}
 		}
 	}
-}
 
-class Tile {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-		this.isWatered = false;
-		this.state = tileState.empty;
-		this.growth = 0;
-	}
-
-	draw() {
-		if (this.isWatered == false) {
-			drawImage(dryDirt1, this.x, this.y);
-		}
-		if (this.isWatered == true) {
-			drawImage(wetDirt1, this.x, this.y);
-		}
-		if (this.state == tileState.hasSeed) {
-			drawImage(seed, this.x, this.y);
-		}
-		if (this.state == tileState.hasPumpkin) {
-			drawImage(pumpkin, this.x + 10, this.y + 10);
-		}
-	}
-
-	water() {
-		this.isWatered = true;
-	}
-
-	plant() {
-		this.isWatered = false;
-		this.state = tileState.hasSeed;
-	}
-
-	grow() {
-		if (this.isWatered == true && this.state == tileState.hasSeed) {
-			this.growth += 0.25;
-		}
-		if (this.growth == 25) {
+	class Tile {
+		constructor(x, y, state) {
+			this.x = x;
+			this.y = y;
 			this.isWatered = false;
+			this.state = state;
+			this.growth = 0;
 		}
-		if (this.growth == 50) {
-			this.isWatered = false;
-		}
-		if (this.growth == 75) {
-			this.isWatered = false;
-		}
-		if (this.growth >= 100) {
-			this.state = tileState.hasPumpkin;
-		}
-	}
 
-	harvest() {
-		this.state = tileState.empty;
-		this.isWatered = false;
-		this.growth = 0;
-	}
-}
-
-const init = () => {
-	pumpkin.src = "Assets/Pumpkin1.png";
-	seedBag.src = "Assets/SeedBag.png";
-	dryDirt1.src = "Assets/DryDirt1.png";
-	wetDirt1.src = "Assets/WetDirt1.png";
-	ghost.src = "Assets/Ghost.png";
-	seed.src = "Assets/Seed.png";
-	for (let x = 50; x < 750; x += 50) {
-		for (let y = 50; y < 750; y += 50) {
-			tiles.push(new Tile(x, y));
-		}
-	}
-	ghosts.push(new Ghost(-50, -50, -50, -50));
-};
-
-const drawScene = () => {
-	clear(); // Run every frame.
-	for (let tile of tiles) {
-		tile.draw();
-
-		if (Collision.isPointInRec(mouseX, mouseY, tile)) {
-			drawRectangeLines(tile.x, tile.y, 50, 50, "#ffffeb");
-		}
-	}
-	for (let ghost of ghosts) {
-		ghost.draw();
-	}
-
-	drawText(`Seeds: ${seeds}`, 50, 785, 16, "#ffb570");
-	drawImage(seedBag, 0, 750);
-	drawText(`Pumpkins: ${pumpkins}`, 550, 785, 16, "#ffb570");
-	drawImage(pumpkin, 500, 760);
-};
-
-const updateScene = () => {
-	drawScene();
-	// Handle tile state.
-	if (mouseReleased) {
-		let currentTile = tiles.find((x) =>
-			Collision.isPointInRec(mouseX, mouseY, x)
-		);
-		if (currentTile.state == tileState.empty && currentTile.isWatered) {
-			if (seeds > 0) {
-				currentTile.plant();
-				seeds -= 1;
-				mouseReleased = false;
+		draw() {
+			if (this.isWatered == false) {
+				drawImage(dryDirt1, this.x, this.y);
+			}
+			if (this.isWatered == true) {
+				drawImage(wetDirt1, this.x, this.y);
+			}
+			switch (this.state) {
+				case tileState.hasSeed:
+					drawImage(seed, this.x, this.y);
+					break;
+				case tileState.hasPumpkin:
+					drawImage(pumpkin, this.x + 10, this.y + 10);
+					break;
+				case tileState.isWell:
+					drawText("W", this.x + 15, this.y + 35, 22, "#606070");
+					break;
+				case tileState.hasTree:
+					drawText("T", this.x + 15, this.y + 35, 22, "#964253");
+					break;
+				case tileState.hasStone:
+					drawText("S", this.x + 15, this.y + 35, 22, "#c2c2d1");
+					break;
 			}
 		}
-		if (currentTile.state == tileState.hasPumpkin) {
-			seeds += 2;
-			pumpkins += 1;
-			currentTile.harvest();
-			mouseReleased = false;
-		} else {
-			currentTile.water();
-			mouseReleased = false;
+
+		water() {
+			this.isWatered = true;
+		}
+
+		plant() {
+			this.isWatered = false;
+			this.state = tileState.hasSeed;
+		}
+
+		grow() {
+			if (this.isWatered == true && this.state == tileState.hasSeed) {
+				this.growth += 0.25;
+			}
+			if (this.growth == 25) {
+				this.isWatered = false;
+			}
+			if (this.growth == 50) {
+				this.isWatered = false;
+			}
+			if (this.growth == 75) {
+				this.isWatered = false;
+			}
+			if (this.growth >= 100) {
+				this.state = tileState.hasPumpkin;
+			}
+		}
+
+		harvest() {
+			this.state = tileState.empty;
+			this.isWatered = false;
+			this.growth = 0;
 		}
 	}
-	// Grow
-	for (let tile of tiles) {
-		tile.grow();
-	}
-	for (let ghost of ghosts) {
-		ghost.search();
-	}
-	if (ghosts.length < 2 && pumpkins == 25) {
-		ghosts.push(new Ghost(850, 850, 850, 850));
-	}
-	if (ghosts.length < 3 && pumpkins == 50) {
-		ghosts.push(new Ghost(850, 0, 850, 0));
-	}
-	if (ghosts.length < 4 && pumpkins == 75) {
-		ghosts.push(new Ghost(0, 850, 0, 850));
-	}
-};
 
+	// Fix this picker.
+	const statePick = () => {
+		let rand = Math.floor(Math.random() * Math.floor(3));
+		switch (rand) {
+			case 0:
+				return tileState.empty;
+			case 1:
+				return tileState.hasStone;
+			case 2:
+				return tileState.hasTree;
+			default:
+				return tileState.empty;
+		}
+	};
+
+	const incTime = () => {
+		tick += 1;
+		if (tick > 100) {
+			seconds -= 1;
+			tick = 0;
+		}
+	};
+
+	const calcScore = () => {
+		score = pumpkins * 100;
+	};
+
+	const init = () => {
+		pumpkin.src = "Assets/Pumpkin1.png";
+		seedBag.src = "Assets/SeedBag.png";
+		dryDirt1.src = "Assets/DryDirt1.png";
+		wetDirt1.src = "Assets/WetDirt1.png";
+		ghost.src = "Assets/Ghost.png";
+		seed.src = "Assets/Seed.png";
+		for (let x = 50; x < 750; x += 50) {
+			for (let y = 50; y < 750; y += 50) {
+				tiles.push(new Tile(x, y, tileState.empty));
+			}
+		}
+
+		//TODO: randomly add these properties.
+		for (let tile of tiles) {
+			tile.state = statePick();
+		}
+
+		tiles[105].state = tileState.isWell;
+		ghosts.push(new Ghost(-50, -50, -50, -50));
+		meter = new WaterMeter();
+
+	};
+
+	const drawScene = () => {
+		clear(); // Run every frame.
+		for (let tile of tiles) {
+			tile.draw();
+
+			if (Collision.isPointInRec(mouseX, mouseY, tile)) {
+				drawRectangeLines(tile.x, tile.y, 50, 50, "#ffffeb");
+			}
+		}
+		for (let ghost of ghosts) {
+			ghost.draw();
+		}
+
+		drawText(`Time: ${seconds}`, 50, 30 , 16, "#ffb570");
+		drawText(`Score: ${score}`, 550, 30, 16, "#ffb570");
+		drawText(`Seeds: ${seeds}`, 50, 785, 16, "#ffb570");
+		drawImage(seedBag, 0, 750);
+		drawText(`Pumpkins: ${pumpkins}`, 550, 785, 16, "#ffb570");
+		drawImage(pumpkin, 500, 760);
+		meter.draw();
+	};
+
+	const updateScene = () => {
+		incTime();
+		calcScore();
+		if (seconds > 0) {
+			drawScene();
+			// Handle tile state.
+			if (mouseReleased) {
+				let currentTile = tiles.find((x) =>
+					Collision.isPointInRec(mouseX, mouseY, x)
+				);
+				if (currentTile.state == tileState.empty && currentTile.isWatered) {
+					if (seeds > 0) {
+						currentTile.plant();
+						seeds -= 1;
+						mouseReleased = false;
+					}
+				}
+				if (currentTile.state == tileState.hasPumpkin) {
+					seeds += 2;
+					pumpkins += 1;
+					currentTile.harvest();
+					mouseReleased = false;
+				}
+				if (currentTile.state == tileState.isWell) {
+					meter.fill();
+					mouseReleased = false;
+
+				} else if (currentTile.state != tileState.hasTree && currentTile.state != tileState.hasStone) {
+					if (meter.fillLevel < 0 && currentTile.isWatered == false) {
+						currentTile.water();
+						meter.drain();
+					}
+					mouseReleased = false;
+				}
+				else {
+					mouseReleased = false;
+				}
+			}
+			// Grow
+			for (let tile of tiles) {
+				tile.grow();
+			}
+			for (let ghost of ghosts) {
+				ghost.search();
+			}
+			if (ghosts.length < 2 && pumpkins == 25) {
+				ghosts.push(new Ghost(850, 850, 850, 850));
+			}
+			if (ghosts.length < 3 && pumpkins == 50) {
+				ghosts.push(new Ghost(850, 0, 850, 0));
+			}
+			if (ghosts.length < 4 && pumpkins == 75) {
+				ghosts.push(new Ghost(0, 850, 0, 850));
+			}
+		}
+	};
+
+	init();
+	setInterval(updateScene, 10);
+};
 // init ; update -> draw
-init();
-setInterval(updateScene, 10);
+game();
