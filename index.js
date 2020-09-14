f1e06cc042e5ae33fb1bc46d301a33f6c2a7c19d
markusreynolds1989 <markusreynolds1989@gmail.com>
2020-09-14 10:55:35 -0500

    Adjusted items

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ let rightPressed = false,
 	mouseY;
 
 /***  Library functions ***/
+const drawImage = (sprite, x, y) => ctx.drawImage(sprite, x, y);
+
 const drawText = (text, x, y, size, color) => {
 	ctx.fillStyle = color;
 	ctx.font = `${size}pt mono`;
@@ -45,37 +47,29 @@ const clear = () => {
 };
 
 const keyDownHandler = (event) => {
-	if (event.key == "Right"
-		|| event.key == "ArrowRight") {
+	if (event.key == "Right" || event.key == "ArrowRight") {
 		rightPressed = true;
 		event.preventDefault();
-	} else if (event.key == "Left"
-		|| event.key == "ArrowLeft") {
+	} else if (event.key == "Left" || event.key == "ArrowLeft") {
 		leftPressed = true;
 		event.preventDefault();
-	} else if (event.key == "Up"
-		|| event.key == "ArrowUp") {
+	} else if (event.key == "Up" || event.key == "ArrowUp") {
 		upPressed = true;
 		event.preventDefault();
-	} else if (event.key == "Down"
-		|| event.key == "ArrowDown") {
+	} else if (event.key == "Down" || event.key == "ArrowDown") {
 		downPressed = true;
 		event.preventDefault();
 	}
 };
 
 const keyUpHandler = (event) => {
-	if (event.key == "Right"
-		|| event.key == "ArrowRight") {
+	if (event.key == "Right" || event.key == "ArrowRight") {
 		rightPressed = false;
-	} else if (event.key == "Left"
-		|| event.key == "ArrowLeft") {
+	} else if (event.key == "Left" || event.key == "ArrowLeft") {
 		leftPressed = false;
-	} else if (event.key == "Up"
-		|| event.key == "ArrowUp") {
+	} else if (event.key == "Up" || event.key == "ArrowUp") {
 		upPressed = false;
-	} else if (event.key == "Down"
-		|| event.key == "ArrowDown") {
+	} else if (event.key == "Down" || event.key == "ArrowDown") {
 		downPressed = false;
 	}
 };
@@ -91,17 +85,21 @@ const mouseClickHandler = () => {
 
 class Collision {
 	static isRecTouching(item1, item2) {
-		return item1.x < (item2.x + item2.dx)
-			&& (item1.x + item1.dx) > item2.x
-			&& item1.y < (item2.y + item2.dy)
-			&& (item1.y + item1.dy) > item2.y;
+		return (
+			item1.x < item2.x + item2.dx &&
+			item1.x + item1.dx > item2.x &&
+			item1.y < item2.y + item2.dy &&
+			item1.y + item1.dy > item2.y
+		);
 	}
 
 	static isPointInRec(pointX, pointY, item2) {
-		return (pointX >= item2.x
-			&& pointX <= item2.x + 50
-			&& pointY >= item2.y
-			&& pointY <= item2.y + 50);
+		return (
+			pointX >= item2.x &&
+			pointX <= item2.x + 50 &&
+			pointY >= item2.y &&
+			pointY <= item2.y + 50
+		);
 	}
 }
 
@@ -111,7 +109,6 @@ document.addEventListener("mousemove", mouseMoveHandler, false);
 document.addEventListener("mouseup", mouseClickHandler, false);
 
 /*** End Library  ***/
-
 
 // Start Program
 const pumpkin = new Image(),
@@ -123,7 +120,7 @@ const pumpkin = new Image(),
 	tileState = {
 		empty: "empty",
 		hasSeed: "hasSeed",
-		hasPumpkin: "hasPumpkin"
+		hasPumpkin: "hasPumpkin",
 	};
 
 let tiles = [],
@@ -140,11 +137,11 @@ class Ghost {
 	}
 
 	draw() {
-		ctx.drawImage(ghost, this.x, this.y);
+		drawImage(ghost, this.x, this.y);
 	}
 
 	search() {
-		let currentTile = tiles.find(x => x.state == tileState.hasPumpkin);
+		let currentTile = tiles.find((x) => x.state == tileState.hasPumpkin);
 		if (currentTile != undefined) {
 			if (this.x < currentTile.x) {
 				this.x += 0.5;
@@ -158,17 +155,24 @@ class Ghost {
 			if (this.y > currentTile.y) {
 				this.y -= 0.5;
 			}
-			if (this.x == currentTile.x
-				&& this.y == currentTile.y) {
+			if (this.x == currentTile.x && this.y == currentTile.y) {
 				currentTile.harvest();
 			}
-		}
-		else {
-			if (this.x > this.originX) {
-				this.x -= 1;
-			}
-			if (this.y > this.originY) {
-				this.y -= 1;
+		} else {
+			if (this.originX > 0 && this.originY > 0) {
+				if (this.x < this.originX) {
+					this.x += 1;
+				}
+				if (this.y < this.originY) {
+					this.y += 1;
+				}
+			} else {
+				if (this.x > this.originX) {
+					this.x -= 1;
+				}
+				if (this.y > this.originY) {
+					this.y -= 1;
+				}
 			}
 		}
 	}
@@ -185,16 +189,16 @@ class Tile {
 
 	draw() {
 		if (this.isWatered == false) {
-			ctx.drawImage(dryDirt1, this.x, this.y);
+			drawImage(dryDirt1, this.x, this.y);
 		}
 		if (this.isWatered == true) {
-			ctx.drawImage(wetDirt1, this.x, this.y);
+			drawImage(wetDirt1, this.x, this.y);
 		}
 		if (this.state == tileState.hasSeed) {
-			ctx.drawImage(seed, this.x, this.y);
+			drawImage(seed, this.x, this.y);
 		}
 		if (this.state == tileState.hasPumpkin) {
-			ctx.drawImage(pumpkin, this.x + 10, this.y + 10);
+			drawImage(pumpkin, this.x + 10, this.y + 10);
 		}
 	}
 
@@ -230,7 +234,6 @@ class Tile {
 		this.isWatered = false;
 		this.growth = 0;
 	}
-
 }
 
 const init = () => {
@@ -245,7 +248,7 @@ const init = () => {
 			tiles.push(new Tile(x, y));
 		}
 	}
-	ghosts.push(new Ghost(-50, -50));
+	ghosts.push(new Ghost(-50, -50, -50, -50));
 };
 
 const drawScene = () => {
@@ -262,16 +265,18 @@ const drawScene = () => {
 	}
 
 	drawText(`Seeds: ${seeds}`, 50, 785, 16, "#ffb570");
-	ctx.drawImage(seedBag, 0, 750);
-	drawText(`Pumpkins: ${pumpkins}`, 650, 785, 16, "#ffb570");
-	ctx.drawImage(pumpkin, 610, 760);
+	drawImage(seedBag, 0, 750);
+	drawText(`Pumpkins: ${pumpkins}`, 550, 785, 16, "#ffb570");
+	drawImage(pumpkin, 500, 760);
 };
 
 const updateScene = () => {
 	drawScene();
 	// Handle tile state.
 	if (mouseReleased) {
-		let currentTile = tiles.find(x => Collision.isPointInRec(mouseX, mouseY, x));
+		let currentTile = tiles.find((x) =>
+			Collision.isPointInRec(mouseX, mouseY, x)
+		);
 		if (currentTile.state == tileState.empty && currentTile.isWatered) {
 			if (seeds > 0) {
 				currentTile.plant();
@@ -284,8 +289,7 @@ const updateScene = () => {
 			pumpkins += 1;
 			currentTile.harvest();
 			mouseReleased = false;
-		}
-		else {
+		} else {
 			currentTile.water();
 			mouseReleased = false;
 		}
@@ -298,7 +302,13 @@ const updateScene = () => {
 		ghost.search();
 	}
 	if (ghosts.length < 2 && pumpkins == 25) {
-		ghosts.push(new Ghost(850, 850));
+		ghosts.push(new Ghost(850, 850, 850, 850));
+	}
+	if (ghosts.length < 3 && pumpkins == 50) {
+		ghosts.push(new Ghost(850, 0, 850, 0));
+	}
+	if (ghosts.length < 4 && pumpkins == 75) {
+		ghosts.push(new Ghost(0, 850, 0, 850));
 	}
 };
 
